8de125148a05d5aa1131dba390fb9a707be829db
HasanRzayev <hsnrz2002@gmail.com>
2022-11-07 12:10:38 +0400

    Add project files.

---
+++ MyProfessionalASP.NETproject/Controllers/ProductController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyProfessionalASP.NETproject.Models;
+
+namespace MyProfessionalASP.NETproject.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new()
+        {
+            new Product{Name= "Uludag limonata ",  Description="Meyve suyu",Price= 10 },
+            new Product{Name= "Kola "  ,  Description=  "Icki", Price= 15 },
+            new Product{Name= "Fanta " ,   Description= "Icki",Price=  5 },
+            new Product{Name= "Uludag limonata ",   Description= "Meyve suyu",Price=  10 },
+            new Product{Name= "Kola " ,   Description= "Icki",Price=  15 },
+            new Product{Name= "Fanta ",  Description=  "Icki",Price=  5 },
+            new Product{Name= "Uludag limonata ",    Description="Meyve suyu",Price=  10 },
+            new Product{Name= "Kola ",   Description= "Icki",Price=  15 },
+            new Product{Name= "Fanta ",   Description= "Icki",Price=  5 },
+    };
+
+        public int number { get; set; }
+        public ProductController()
+        {
+          
+        }
+        public IActionResult All()
+        {
+            ViewBag.number = number;
+            return View(products);
+        }
+
+     
+        public IActionResult Delete() { 
+            return View(); 
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult Delete(int number)
+        {
+            if(number>=0 && number<products.Count())
+            {
+
+               
+                products.RemoveAt(number);
+            }
+            return RedirectToAction("All");
+        }
+        [HttpPost, ActionName("Add")]
+        public IActionResult Add(Product product)
+        {
+            if(product.Name!=null && product.Description!=null && product.Price != null)
+            {
+                products.Add(product);
+                
+            }
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ MyProfessionalASP.NETproject/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace MyProfessionalASP.NETproject.Models
+{
+    public class Product
+    {
+        //public Product(string name, string description, double price)
+        //{
+        //    Name = name;
+        //    Description = description;
+        //    Price = price;
+        //}
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
